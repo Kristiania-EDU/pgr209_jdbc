6dddc1c4728c7c9aebb35537ce550a926a402c7f
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-18 11:41:04 +0200

    🔴 BookDAO: Table books not found

--- src/test/java/no/sebastiannordby/pgr209_jdbc/database/BookDaoTest.java
+++ src/test/java/no/sebastiannordby/pgr209_jdbc/database/BookDaoTest.java
@@ -1,13 +1,21 @@
 package no.sebastiannordby.pgr209_jdbc.database;
 
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookDaoTest {
-    private BookDao dao = new BookDao();
+    private BookDao dao;
+
+    @BeforeEach
+    void setup() {
+        var dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:mem:testDatabase");
+        dao = new BookDao(dataSource);
+    }
 
     @Test
     void shouldRetrieveBook() throws SQLException {
