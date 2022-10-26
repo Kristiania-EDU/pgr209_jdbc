01bcbeaf2bc7f57b9ba3d5a3f5d860d2c0295671
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-26 12:04:22 +0200

    Started JPA

--- src/main/java/no/sebastiannordby/pgr209_jdbc/database/jpa/JpaLibraryDao.java
+++ src/main/java/no/sebastiannordby/pgr209_jdbc/database/jpa/JpaLibraryDao.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JpaLibraryDao implements LibraryDao {
+
     @Override
     public void save(Library library) throws SQLException {
 
